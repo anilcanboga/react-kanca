f960d3f74d48e4ed92b4b6168cde883d5a7f6acf
Anılcan <anilcan@getmobil.com>
2024-08-01 14:30:00 +0300

    useInterval added

---
+++ src/useInterval/index.jsx
@@ -0,0 +1,18 @@
+import { useEffect, useRef } from 'react'
+
+export const useInterval = (callback, delay) => {
+  const savedCallback = useRef(() => {})
+
+  useEffect(() => {
+    savedCallback.current = callback
+  })
+
+  useEffect(() => {
+    if (delay !== null) {
+      const interval = setInterval(() => savedCallback.current(), delay || 0)
+      return () => clearInterval(interval)
+    }
+
+    return undefined
+  }, [delay])
+}
