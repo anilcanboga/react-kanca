de023d3b11718e778bf8696b1f9dbd762d07aeeb
Anılcan <anilcan@getmobil.com>
2024-08-01 14:30:42 +0300

    useWindowScroll added

---
+++ src/useWindowScroll/index.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react'
+import { isBrowser, off, on } from '../utils/util'
+
+export const useWindowScroll = () => {
+  const [state, setState] = useState(() => ({
+    x: isBrowser ? window.pageXOffset : 0,
+    y: isBrowser ? window.pageYOffset : 0
+  }))
+
+  useEffect(() => {
+    const handler = () => {
+      setState((state) => {
+        const { pageXOffset, pageYOffset } = window
+        //Check state for change, return same state if no change happened to prevent rerender
+        //(see useState/setState documentation). useState/setState is used internally in useRafState/setState.
+        return state.x !== pageXOffset || state.y !== pageYOffset
+          ? {
+              x: pageXOffset,
+              y: pageYOffset
+            }
+          : state
+      })
+    }
+
+    //We have to update window scroll at mount, before subscription.
+    //Window scroll may be changed between render and effect handler.
+    handler()
+
+    on(window, 'scroll', handler, {
+      capture: false,
+      passive: true
+    })
+
+    return () => {
+      off(window, 'scroll', handler)
+    }
+  }, [])
+
+  return state
+}
