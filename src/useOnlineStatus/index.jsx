62466d12c85e980f51546f7a7600a65c5af18c48
Anılcan <anilcan@getmobil.com>
2024-08-01 14:30:21 +0300

    useOnlineStatus

---
+++ src/useOnlineStatus/index.jsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react'
+
+export const useOnlineStatus = () => {
+  const [isOnline, setIsOnline] = useState(navigator.onLine)
+
+  useEffect(() => {
+    const handleOnline = () => setIsOnline(true)
+    const handleOffline = () => setIsOnline(false)
+
+    window.addEventListener('online', handleOnline)
+    window.addEventListener('offline', handleOffline)
+
+    return () => {
+      window.removeEventListener('online', handleOnline)
+      window.removeEventListener('offline', handleOffline)
+    }
+  }, [])
+
+  return isOnline
+}
