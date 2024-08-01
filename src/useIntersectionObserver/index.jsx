d69bd634edad5bd66522d25170efd63c7ec76f51
Anılcan <anilcan@getmobil.com>
2024-08-01 14:29:47 +0300

    useIntersectionObserver added

---
+++ src/useIntersectionObserver/index.jsx
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react'
+
+export const useIntersectionObserver = (targetRef, options) => {
+  const [isIntersecting, setIsIntersecting] = useState(false)
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) => {
+      setIsIntersecting(entry.isIntersecting)
+    }, options)
+
+    if (targetRef.current) {
+      observer.observe(targetRef.current)
+    }
+
+    return () => {
+      if (targetRef.current) {
+        observer.unobserve(targetRef.current)
+      }
+    }
+  }, [targetRef, options])
+
+  return isIntersecting
+}
