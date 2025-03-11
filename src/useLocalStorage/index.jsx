2f6eab55e65c23e7cfc67feece3154fa6914c6e5
anilcanboga <anlcn.boga@hotmail.com>
2025-03-11 15:18:35 +0300

    useLocalStorage updated

--- src/useLocalStorage/index.jsx
+++ src/useLocalStorage/index.jsx
@@ -1,7 +1,7 @@
 import { useCallback, useState, useRef, useLayoutEffect } from 'react'
 import { isBrowser, noop } from '../utils/util'
 
-export const useLocalStorage = (key, initialValue, options) => {
+export const useLocalStorage = (key, initialValue = undefined, options) => {
   if (!isBrowser) {
     return [initialValue, noop, noop]
   }
@@ -9,32 +9,26 @@ export const useLocalStorage = (key, initialValue, options) => {
     throw new Error('useLocalStorage key may not be falsy')
   }
 
-  const deserializer = options
-    ? options.raw
-      ? (value) => value
-      : options.deserializer
-    : JSON.parse
+  const deserializer = options?.raw
+    ? (value) => value
+    : options?.deserializer || JSON.parse
 
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const initializer = useRef((key) => {
     try {
-      const serializer = options
-        ? options.raw
-          ? String
-          : options.serializer
-        : JSON.stringify
+      const serializer = options?.raw
+        ? String
+        : options?.serializer || JSON.stringify
 
       const localStorageValue = localStorage.getItem(key)
       if (localStorageValue !== null) {
         return deserializer(localStorageValue)
-      } else {
-        initialValue && localStorage.setItem(key, serializer(initialValue))
+      } else if (initialValue !== undefined) {
+        localStorage.setItem(key, serializer(initialValue))
         return initialValue
       }
+      return undefined
     } catch {
-      // If user is in private mode or has storage restriction
-      // localStorage can throw. JSON.parse and JSON.stringify
-      // can throw, too.
       return initialValue
     }
   })
@@ -43,30 +37,40 @@ export const useLocalStorage = (key, initialValue, options) => {
   const [state, setState] = useState(() => initializer.current(key))
 
   // eslint-disable-next-line react-hooks/rules-of-hooks
-  useLayoutEffect(() => setState(initializer.current(key)), [key])
+  useLayoutEffect(() => {
+    setState(initializer.current(key))
+  }, [key])
 
   // eslint-disable-next-line react-hooks/rules-of-hooks
+  const safeJSONParse = (value) => {
+    try {
+      return JSON.parse(value)
+    } catch {
+      return value
+    }
+  }
+
   const set = useCallback(
     (valOrFunc) => {
       try {
-        const newState =
-          typeof valOrFunc === 'function' ? valOrFunc(state) : valOrFunc
-        if (typeof newState === 'undefined') return
-        let value
+        setState((prev) => {
+          const newState =
+            typeof valOrFunc === 'function' ? valOrFunc(prev) : valOrFunc
+          if (typeof newState === 'undefined') return prev
 
-        if (options)
-          if (options.raw)
-            if (typeof newState === 'string') value = newState
-            else value = JSON.stringify(newState)
-          else if (options.serializer) value = options.serializer(newState)
-          else value = JSON.stringify(newState)
-        else value = JSON.stringify(newState)
+          const value = options?.raw
+            ? String(newState)
+            : options?.serializer
+              ? options.serializer(newState)
+              : typeof newState === 'string'
+                ? newState
+                : JSON.stringify(newState)
 
-        localStorage.setItem(key, value)
-        setState(deserializer(value))
+          localStorage.setItem(key, value)
+          return safeJSONParse(value)
+        })
       } catch {
-        // If user is in private mode or has storage restriction
-        // localStorage can throw. Also JSON.stringify can throw.
+        // Hata yönetimi
       }
     },
     [key, setState]
@@ -78,8 +82,7 @@ export const useLocalStorage = (key, initialValue, options) => {
       localStorage.removeItem(key)
       setState(undefined)
     } catch {
-      // If user is in private mode or has storage restriction
-      // localStorage can throw.
+      // localStorage hatası yönetimi
     }
   }, [key, setState])
 
