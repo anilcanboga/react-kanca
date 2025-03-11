b5c912526c57cfacdc9803c529a3f141bcc8ce56
anilcanboga <anlcn.boga@hotmail.com>
2025-03-11 15:18:55 +0300

    useSessionStorage updated

--- src/useSessionStorage/index.jsx
+++ src/useSessionStorage/index.jsx
@@ -1,26 +1,68 @@
-import { useState } from 'react'
+import { useState, useCallback } from 'react'
 
-export const useSessionStorage = (key, initialValue) => {
-  // İlk değeri sessionStorage'den alıyoruz, yoksa belirlenen başlangıç değerini kullanıyoruz
+const safeJSONParse = (value) => {
+  try {
+    return JSON.parse(value)
+  } catch {
+    return value // Eğer JSON formatı değilse, direkt string olarak döndür
+  }
+}
+
+export const useSessionStorage = (key, initialValue, options) => {
+  if (!key) {
+    throw new Error('useSessionStorage key may not be falsy')
+  }
+
+  const deserializer = options?.raw ? (value) => value : safeJSONParse
+
+  const serializer = options?.raw
+    ? String
+    : (value) => (typeof value === 'string' ? value : JSON.stringify(value))
+
   const [storedValue, setStoredValue] = useState(() => {
     try {
       const item = window.sessionStorage.getItem(key)
-      return item ? JSON.parse(item) : initialValue
-    } catch (error) {
-      console.error(error)
+      if (item !== null) {
+        return deserializer(item)
+      } else if (initialValue !== undefined) {
+        // Eğer initialValue varsa sessionStorage'a yaz
+        window.sessionStorage.setItem(key, serializer(initialValue))
+        return initialValue
+      }
+      return undefined
+    } catch {
       return initialValue
     }
   })
 
-  // State güncellendiğinde, sessionStorage'ye de yazıyoruz
-  const setValue = (value) => {
-    try {
-      setStoredValue(value)
-      window.sessionStorage.setItem(key, JSON.stringify(value))
-    } catch (error) {
-      console.error(error)
-    }
-  }
+  const setValue = useCallback(
+    (valueOrFunc) => {
+      try {
+        setStoredValue((prev) => {
+          const newValue =
+            typeof valueOrFunc === 'function' ? valueOrFunc(prev) : valueOrFunc
 
-  return [storedValue, setValue]
+          if (typeof newValue === 'undefined') return prev
+
+          const valueToStore = serializer(newValue)
+          window.sessionStorage.setItem(key, valueToStore)
+          return deserializer(valueToStore)
+        })
+      } catch {
+        // Hata yönetimi
+      }
+    },
+    [key, deserializer, serializer]
+  )
+
+  const remove = useCallback(() => {
+    try {
+      window.sessionStorage.removeItem(key)
+      setStoredValue(undefined)
+    } catch {
+      // Hata yönetimi
+    }
+  }, [key])
+
+  return [storedValue, setValue, remove]
 }
