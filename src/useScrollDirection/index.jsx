35d82b51e61441b299419753b9aeaf3c7dd3f290
anilcanboga <anlcn.boga@hotmail.com>
2025-07-27 15:29:58 +0300

    useScrollDirection hook added

---
+++ src/useScrollDirection/index.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react"
+
+export function useScrollDirection({
+  threshold = 10,
+  initialDirection = "down"
+} = {}) {
+  const [scrollDir, setScrollDir] = useState(initialDirection)
+
+  useEffect(() => {
+    let lastScrollY = window.scrollY
+    let ticking = false
+
+    const updateScrollDir = () => {
+      const scrollY = window.scrollY
+
+      if (Math.abs(scrollY - lastScrollY) < threshold) {
+        ticking = false
+        return
+      }
+
+      const direction = scrollY > lastScrollY ? "down" : "up"
+      setScrollDir(direction)
+      lastScrollY = scrollY > 0 ? scrollY : 0
+      ticking = false
+    }
+
+    const onScroll = () => {
+      if (!ticking) {
+        window.requestAnimationFrame(updateScrollDir)
+        ticking = true
+      }
+    }
+
+    window.addEventListener("scroll", onScroll)
+    return () => window.removeEventListener("scroll", onScroll)
+  }, [threshold])
+
+  return scrollDir
+}
