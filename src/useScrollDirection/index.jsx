0b03eb5d99c854d31846483fa0dd9e3d60d38dc9
anilcanboga <anlcn.boga@hotmail.com>
2025-08-14 22:23:12 +0300

    useScrollDirection updated

--- src/useScrollDirection/index.jsx
+++ src/useScrollDirection/index.jsx
@@ -1,9 +1,9 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from 'react'
 
-export function useScrollDirection({
+export const useScrollDirection = ({
   threshold = 10,
-  initialDirection = "down"
-} = {}) {
+  initialDirection = 'down'
+} = {}) => {
   const [scrollDir, setScrollDir] = useState(initialDirection)
 
   useEffect(() => {
@@ -18,7 +18,7 @@ export function useScrollDirection({
         return
       }
 
-      const direction = scrollY > lastScrollY ? "down" : "up"
+      const direction = scrollY > lastScrollY ? 'down' : 'up'
       setScrollDir(direction)
       lastScrollY = scrollY > 0 ? scrollY : 0
       ticking = false
@@ -31,8 +31,8 @@ export function useScrollDirection({
       }
     }
 
-    window.addEventListener("scroll", onScroll)
-    return () => window.removeEventListener("scroll", onScroll)
+    window.addEventListener('scroll', onScroll)
+    return () => window.removeEventListener('scroll', onScroll)
   }, [threshold])
 
   return scrollDir
